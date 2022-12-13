a76de9d542cfb422c06b06d5ddace3c051ec152a
Radek Kádner <radek.kadner@gmail.com>
2022-12-13 06:45:42 +0000

    .

--- extras/metricbeat/metricbeat/install.bash
+++ extras/metricbeat/metricbeat/install.bash
@@ -6,14 +6,16 @@
 # docker stop platel-metricbeat
 # docker start platel-metricbeat
 
+rm /usr/share/metricbeat/modules.d/beat-xpack.yml.disabled
+cp /root/bin/beat-xpack.yml /usr/share/metricbeat/modules.d/beat-xpack.yml
 rm /usr/share/metricbeat/modules.d/elasticsearch-xpack.yml.disabled
-cp /root/bin/elasticsearch-xpack.yml  /usr/share/metricbeat/modules.d/elasticsearch-xpack.yml
+cp /root/bin/elasticsearch-xpack.yml /usr/share/metricbeat/modules.d/elasticsearch-xpack.yml
 rm /usr/share/metricbeat/modules.d/kibana-xpack.yml.disabled
-cp /root/bin/kibana-xpack.yml  /usr/share/metricbeat/modules.d/kibana-xpack.yml
+cp /root/bin/kibana-xpack.yml /usr/share/metricbeat/modules.d/kibana-xpack.yml
 rm /usr/share/metricbeat/modules.d/logstash-xpack.yml.disabled
-cp /root/bin/logstash-xpack.yml  /usr/share/metricbeat/modules.d/logstash-xpack.yml
+cp /root/bin/logstash-xpack.yml /usr/share/metricbeat/modules.d/logstash-xpack.yml
 rm /usr/share/metricbeat/modules.d/system.yml
-cp /root/bin/system.yml  /usr/share/metricbeat/modules.d/systemc.yml
+cp /root/bin/system.yml /usr/share/metricbeat/modules.d/system.yml
 rm /usr/share/metricbeat/modules.d/docker.yml.disabled
-cp /root/bin/docker.yml  /usr/share/metricbeat/modules.d/docker.yml
+cp /root/bin/docker.yml /usr/share/metricbeat/modules.d/docker.yml
 chown root.root /usr/share/metricbeat/modules.d/*
