6e7f0d37aeb3221ced36fb391b7d6b12775e14b0
Radek Kádner <radek.kadner@gmail.com>
2022-12-15 04:47:24 +0100

    .

--- extras/metricbeat/metricbeat/install.bash
+++ extras/metricbeat/metricbeat/install.bash
@@ -15,7 +15,7 @@ cp /root/bin/kibana-xpack.yml /usr/share/metricbeat/modules.d/kibana-xpack.yml
 rm /usr/share/metricbeat/modules.d/logstash-xpack.yml.disabled
 cp /root/bin/logstash-xpack.yml /usr/share/metricbeat/modules.d/logstash-xpack.yml
 rm /usr/share/metricbeat/modules.d/system.yml
-cp /root/bin/system.yml /usr/share/metricbeat/modules.d/system.yml
+cp /root/bin/system.yml.disabled /usr/share/metricbeat/modules.d/system.yml.disabled
 rm /usr/share/metricbeat/modules.d/docker.yml.disabled
 cp /root/bin/docker.yml /usr/share/metricbeat/modules.d/docker.yml
 chown root.root /usr/share/metricbeat/modules.d/*
