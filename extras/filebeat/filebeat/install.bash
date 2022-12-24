b9b0bac93171a48d70d01609f98504a4f0722607
Radek Kádner <radek.kadner@gmail.com>
2022-12-24 02:25:37 +0100

    .

--- extras/filebeat/filebeat/install.bash
+++ extras/filebeat/filebeat/install.bash
@@ -15,6 +15,7 @@ cp /root/bin/kibana.yml  /usr/share/filebeat/modules.d/kibana.yml
 rm -f /usr/share/filebeat/modules.d/traefik.yml
 rm -f /usr/share/filebeat/modules.d/traefik.yml.disabled
 cp /root/bin/traefik.yml  /usr/share/filebeat/modules.d/traefik.yml
+rm -f /usr/share/filebeat/modules.d/nginx.yml
 rm -f /usr/share/filebeat/modules.d/nginx.yml.disabled
 cp /root/bin/nginx.yml  /usr/share/filebeat/modules.d/nginx.yml
 chown root.root /usr/share/filebeat/modules.d/*
