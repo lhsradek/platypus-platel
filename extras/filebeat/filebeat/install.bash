1ffb464c03c503ee1955bca9202e133391ef6341
Radek Kádner <radek.kadner@gmail.com>
2022-12-25 19:48:19 +0100

    .

--- extras/filebeat/filebeat/install.bash
+++ extras/filebeat/filebeat/install.bash
@@ -8,14 +8,14 @@
 
 rm -f /usr/share/filebeat/modules.d/elasticsearch.yml
 rm -f /usr/share/filebeat/modules.d/elasticsearch.yml.disabled
-cp /root/bin/elasticsearch.yml  /usr/share/filebeat/modules.d/elasticsearch.yml.disabled
+cp /root/bin/elasticsearch.yml /usr/share/filebeat/modules.d/elasticsearch.yml.disabled
 rm -f /usr/share/filebeat/modules.d/kibana.yml
 rm -f /usr/share/filebeat/modules.d/kibana.yml.disabled
-cp /root/bin/kibana.yml  /usr/share/filebeat/modules.d/kibana.yml
+cp /root/bin/kibana.yml /usr/share/filebeat/modules.d/kibana.yml
 rm -f /usr/share/filebeat/modules.d/traefik.yml
 rm -f /usr/share/filebeat/modules.d/traefik.yml.disabled
-cp /root/bin/traefik.yml  /usr/share/filebeat/modules.d/traefik.yml
+cp /root/bin/traefik.yml /usr/share/filebeat/modules.d/traefik.yml
 rm -f /usr/share/filebeat/modules.d/nginx.yml
 rm -f /usr/share/filebeat/modules.d/nginx.yml.disabled
-cp /root/bin/nginx.yml  /usr/share/filebeat/modules.d/nginx.yml
+cp /root/bin/nginx.yml /usr/share/filebeat/modules.d/nginx.yml
 chown root.root /usr/share/filebeat/modules.d/*
